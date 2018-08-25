bdd1be401ea512fa86b379791f887b5956389cec
federicopopoff <popofffederico@gmail.com>
2018-08-25 09:52:30 -0300

    Tests.

--- ATM.Test/OperationServiceTest.cs
+++ ATM.Test/OperationServiceTest.cs
@@ -12,6 +12,7 @@ namespace ATM.Test
     [TestClass]
     public class OperationServiceTest
     {
+        private OperationService _opService;
         private Mock<IRepository<Operation>> _opRepository;
         private Mock<IRepository<CreditCard>> _ccRepository;
 
@@ -20,12 +21,46 @@ namespace ATM.Test
         {
             this._opRepository = new Mock<IRepository<Operation>>();
             this._ccRepository = new Mock<IRepository<CreditCard>>();
+            this._opService = new OperationService(this._ccRepository.Object, this._opRepository.Object);
+
         }
 
         [TestMethod]
         public void VerifyInsertBalanceCallToInsertOperationTest()
         {
-            throw new System.NotImplementedException();
+            this._opService.InsertBalance(1);
+
+            this._opRepository.Verify(op => op.UpSertEntity(It.Is<Operation>(o => o.CreditCardId == 1)));
+        }
+
+        [TestMethod]
+        public void GetMoreAmountThanTheUserHasTest()
+        {
+            var creditCard = new CreditCard
+            {
+                Id = 1,
+                Balance = 250
+            };
+
+            this._ccRepository.Setup(cc => cc.GetById(1)).Returns(creditCard);
+
+            Assert.ThrowsException<ATMNotCreditException>(() => this._opService.InsertWithDrawal(1, 2000));
+        }
+
+        [TestMethod]
+        public void GetLessAmountThanBalanceTest()
+        {
+            var creditCard = new CreditCard
+            {
+                Id = 1,
+                Balance = 250
+            };
+
+            this._ccRepository.Setup(cc => cc.GetById(1)).Returns(creditCard);
+
+            this._opService.InsertWithDrawal(1, 20);
+
+            this._ccRepository.Verify(op => op.UpSertEntity(It.Is<CreditCard>(o => o.Balance == 230)));
         }
     }
 }
